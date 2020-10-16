4562256ad390e0d1a99c9cdf52ef4f8fa47cc49b
cyd <245692559@qq.com>
2020-10-16 17:23:33 +0800

    引入高德地图

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        首页
+  <div id="app">
+    <div class="amap-wrapper">
+      <el-amap class="amapDemo" vid="amapDemo" :center="center" :zoom="zoom"></el-amap>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  name:"Index",
+  data() {
+    return {
+      zoom:12,
+      center:[116.404765,39.918052]
+    };
+  }
+};
 </script>
 <style lang="scss">
-
+.amap-wrapper {
+  height: 100vh;
+}
 </style>
